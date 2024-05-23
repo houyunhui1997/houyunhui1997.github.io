b63ac0629a3e6982c3b9f6a1d6192139aee8b4f0
hyh <1225118281@qq.com>
2024-05-23 23:05:55 +0800

    Site updated: 2024-05-23 23:05:54

--- js/anzhiyu.js
+++ js/anzhiyu.js
@@ -1,4 +1,6 @@
 var navMusicEl = document.getElementById("nav-music");
+var changeMusicListFlag = true;
+var selectRandomSong = [];
 var anzhiyu = {
   //切换音乐播放状态
   musicToggle: function (changePaly = true) {
@@ -114,7 +116,7 @@ var anzhiyu = {
       , o = t.querySelector("#anMusicRefreshBtn")
       , i = t.querySelector("#anMusicSwitching")
       , r = t.querySelector("meting-js").aplayer;
-    r.volume(.8, !0),
+    r.volume(.2, !0),
     r.on("loadeddata", function() {
         anzhiyu.changeMusicBg()
     }),
@@ -127,10 +129,10 @@ var anzhiyu = {
         "/life/music/" != window.location.pathname ? document.getElementById("menu-mask").removeEventListener("click", e) : t.querySelector(".aplayer-list").classList.remove("aplayer-list-hide")
     }),
     n.addEventListener("click", function() {
-        var e, t;
-        changeMusicListFlag ? (t = (e = document.getElementById("anMusic-page").querySelector("meting-js").aplayer).list.audios,
-        t = Math.floor(Math.random() * t.length),
-        e.list.switch(t)) : anzhiyu.cacheAndPlayMusic()
+        // var e, t;
+        // changeMusicListFlag ? (t = (e = document.getElementById("anMusic-page").querySelector("meting-js").aplayer).list.audios,
+        // t = Math.floor(Math.random() * t.length),
+        // e.list.switch(t)) : anzhiyu.cacheAndPlayMusic()
     }),
     o.addEventListener("click", function() {
         localStorage.removeItem("musicData"),
@@ -153,10 +155,142 @@ var anzhiyu = {
         r.volume(musicVolume, !0))
     })
 },
+changeMusicList: function() {
+  var e = _asyncToGenerator(_regeneratorRuntime().mark(function e() {
+      var t, n, o, i, r;
+      return _regeneratorRuntime().wrap(function(e) {
+          for (; ; )
+              switch (e.prev = e.next) {
+              case 0:
+                  t = document.getElementById("anMusic-page"),
+                  t = t.querySelector("meting-js").aplayer,
+                  n = (new Date).getTime(),
+                  o = JSON.parse(localStorage.getItem("musicData")) || {
+                      timestamp: 0
+                  },
+                  i = [],
+                  changeMusicListFlag ? (i = defaultPlayMusicList,
+                  e.next = 23) : e.next = 9;
+                  break;
+              case 9:
+                  defaultPlayMusicList = t.list.audios,
+                  n - o.timestamp < 864e5 ? (i = o.songs,
+                  e.next = 23) : e.next = 14;
+                  break;
+              case 14:
+                  return e.next = 16,
+                  fetch("/json/music.json");
+              case 16:
+                  return r = e.sent,
+                  e.next = 19,
+                  r.json();
+              case 19:
+                  i = e.sent,
+                  o.timestamp = n,
+                  o.songs = i,
+                  localStorage.setItem("musicData", JSON.stringify(o));
+              case 23:
+                  t.list.clear(),
+                  t.list.add(i),
+                  changeMusicListFlag = !changeMusicListFlag;
+              case 26:
+              case "end":
+                  return e.stop()
+              }
+      }, e)
+  }));
+  return function() {
+      return e.apply(this, arguments)
+  }
+},
+cacheAndPlayMusic: function() {
+  if (e = localStorage.getItem("musicData")) {
+      var e = JSON.parse(e);
+      if ((new Date).getTime() - e.timestamp < 864e5)
+          return void anzhiyu.playMusic(e.songs)
+  }
+  fetch("/json/music.json").then(function(e) {
+      return e.json()
+  }).then(function(e) {
+      var t = {
+          timestamp: (new Date).getTime(),
+          songs: e
+      };
+      localStorage.setItem("musicData", JSON.stringify(t))
+      anzhiyu.playMusic(e)
+  })
+},
+playMusic: function(e) {
+  var t = document.getElementById("anMusic-page").querySelector("meting-js").aplayer
+    , n = e[Math.floor(Math.random() * e.length)]
+    , o = t.list.audios;
+  if (selectRandomSong.includes(n.name)) {
+      for (var i, r = !1; !r; ) {
+          var a = e[Math.floor(Math.random() * e.length)];
+          if (selectRandomSong.includes(a.name) || (t.list.add([a]),
+          t.list.switch(o.length),
+          selectRandomSong.push(a.name),
+          r = !0),
+          selectRandomSong.length === e.length)
+              break
+      }
+      r || -1 != (i = o.findIndex(function(e) {
+          return e.name === n.name
+      })) && t.list.switch(i)
+  } else{
+    t.list.add([n]),
+    t.list.switch(o.length),
+    selectRandomSong.push(n.name);
+    new Vue({
+      data: function () {
+        this.$notify({
+          title: "",
+          message: "本次随机歌曲："+n.name,
+          position: 'top-left',
+          offset: 50,
+          showClose: true,
+          type: "success",
+          duration: 5000
+        });
+      }
+    })
+  }
+},
+jayClick: function(){
+  // 隐藏循环播放
+  document.querySelector(".aplayer-icon-loop").style.display = "none";
+  var jayBtn = document.querySelector(".aplayer-time");
+  var newElement = document.createElement('button');
+  newElement.id = "anMusicBtnGetSong";
+  newElement.classList.add("aplayer-icon-jay","aplayer-icon");
+  newElement.setAttribute("title","随机周董单曲，打开异世界的大梦")
+  newElement.innerHTML = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+  <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+  <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="750" height="750" viewBox="0 0 750 750" enable-background="new 0 0 750 750" xml:space="preserve">  <image id="image0" width="750" height="750" x="0" y="0"
+      href="https://tuchuang.voooe.cn/images/2024/05/23/jay.png" ></image>
+  </svg>
+   `
+  jayBtn.appendChild(newElement);
+  var playJayBtn = document.querySelector(".aplayer-icon-jay");
+  playJayBtn.addEventListener("click", function() {
+      anzhiyu.cacheAndPlayMusic()
+  })
+  
+}
 };
 
 // 如果有右键事件 可以在这里写。
 // addRightMenuClickEvent();
+
+// 第一次进入，绑定事件，改背景
+let timer22 = setInterval(() => {
+  const musiccover = document.querySelector("#anMusic-page .aplayer-pic");
+  // 确保player加载完成
+  if (musiccover) {
+    clearInterval(timer22);
+    anzhiyu.jayClick();
+  }
+}, 100);
 
 // 调用
 anzhiyu.changeMusicBg(false);
